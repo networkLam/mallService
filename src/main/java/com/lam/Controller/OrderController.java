e04027488e549d62f4aa6b839949026370ea24c6
lam <491165524@qq.com>
2024-06-09 23:41:12 +0800

    add look up order function

--- src/main/java/com/lam/Controller/OrderController.java
+++ src/main/java/com/lam/Controller/OrderController.java
@@ -8,10 +8,7 @@ import com.lam.pojo.Order;
 import com.lam.pojo.Result;
 import com.lam.pojo.TokenUserInfo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -39,7 +36,6 @@ public class OrderController {
         } catch (Exception e) {
             return Result.error("无查询结果");
         }
-
     }
 
     //    更新订单信息接口(仅能更新状态和快递编号，是订单的ID ，不是订单号order_number
@@ -90,6 +86,20 @@ public class OrderController {
         } catch (Exception e) {
             return Result.error("分页失败");
         }
+    }
+
+    //根据订单状态查询订单数据
+    @RequestMapping("/api/order")
+    public Result orderList(String status,Integer offset){
+       // System.out.println(status+"and"+offset);
+        if(status.equals("wait") || status.equals("sign") || status.equals("refund")){
+            List<Order> orders = orderMapper.orderList(status, offset);
+            return Result.success(orders);
+        }else {
+            return Result.error("关键字出错");
+
+        }
+
 
     }
 
--- src/main/java/com/lam/mapper/OrderMapper.java
+++ src/main/java/com/lam/mapper/OrderMapper.java
@@ -11,22 +11,22 @@ import java.util.List;
 
 @Mapper
 public interface OrderMapper {
-//    查询某张订单的详情数据 根据订单id（order_id）
+    //    查询某张订单的详情数据 根据订单id（order_id）
     @Select("select * from order_details where order_id = #{order_id}")
     public List<OrderDetails> orderDetails(Integer order_id);
 
-//    插入数据
+    //    插入数据
     @Insert("insert into order_details(order_id, pd_id, number, totals) VALUES (#{order_id},#{pd_id},#{number},#{totals})")
     public void insertDetail(OrderDetails orderDetails) throws Exception;
 
 
-//    @Insert("insert into orders(money, amount, time, state, exp_id) VALUES(#{money},#{amount},#{time},#{state},#{exp_id})")
+    //    @Insert("insert into orders(money, amount, time, state, exp_id) VALUES(#{money},#{amount},#{time},#{state},#{exp_id})")
 //    public void insertOrder(Order order);
 //    把所有的order信息写入order表
     @Update("update orders set money=#{money}, amount=#{amount}, state=#{state},exp_id=#{exp_id} ,uid=#{uid},add_id=#{add_id},order_number=#{order_number},address=#{address},phone=#{phone},contacts=#{contacts} where order_id = #{order_id}")
     public void insertOrder(Order order);
 
-//    更新订单状态
+    //    更新订单状态
     public void updateOrder(Order order) throws Exception;
 
     //根据订单号来查询订单
@@ -36,10 +36,11 @@ public interface OrderMapper {
     //分页浏览订单
     @Select("select order_id, money, amount, time, state, exp_id, order_number, uid, add_id, address, phone, contacts from orders limit 15 offset #{start};")
     public List<Order> orderBrowse(Integer start) throws Exception;
-
     //根据订单ID搜出订单号order_number
 
     @Select("select order_number from orders where order_id = #{order_id}")
     public String searchId(Integer order_id);
 
+    @Select("select order_id, money, amount, time, state, exp_id, order_number, uid, add_id, address, phone, contacts from orders where state=#{status} limit 10 offset #{offset}")
+    public List<Order> orderList(String status, Integer offset);
 }
